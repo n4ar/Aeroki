9424854d2d2b8122823d74195edf46bc57c841c7
Tanathip Nualsuwan <tanathip_it@hotmail.com>
2025-09-21 23:37:48 +0700

    Update Aeroki.c

--- Aeroki/Aeroki.c
+++ Aeroki/Aeroki.c
@@ -1,14 +1,11 @@
 #include "Aeroki.h"
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[]){
     if (argc == 1) {
         __Ark_Shell();
         printf("Exited Shell Mode\n");
         return 0;
     }
-
-    for (size_t Idx = 0; Idx < argc; ++Idx) {
+    for (size_t Idx = 0; Idx < argc; ++Idx){
     }
-
 }
