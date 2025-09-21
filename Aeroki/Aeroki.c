4f17c60858e6efc076ed85196aa2c05140d53841
MiracleOverfl0w <fiw.contact.work@gmail.com>
2025-09-21 16:17:15 +0700

    Structure a project and create some files in Aeroki Folder

---
+++ Aeroki/Aeroki.c
@@ -0,0 +1,16 @@
+#include "Aeroki.h"
+int main(int argc,char * argv[]){
+
+    
+    if(argc == 1){
+        __Ark_Shell();
+        printf("Exited Shell Mode");   
+        return 0; 
+    }
+    
+    for(size_t Idx = 0;Idx < argc ; ++Idx){
+
+    }
+
+
+
---
+++ Aeroki/Aeroki.h
@@ -0,0 +1,12 @@
+#ifndef __AEROKI__
+#define __AEROKI__
+
+// === Include STD Library ===
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void __Ark_Shell();
+void __Ark_Interpreted(FILE *__src_file);
+
+#endif
