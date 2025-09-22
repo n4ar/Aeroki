3aff839941736a32ae203369683bc960b7c4e4ce
Tanathip Nualsuwan <tanathip_it@hotmail.com>
2025-09-22 23:27:52 +0700

    Update Aeroki.c

--- Aeroki/Aeroki.c
+++ Aeroki/Aeroki.c
@@ -103,7 +103,7 @@ void __Ark_Interpreted(FILE *__src_file) {
             handle_hai(line);
         } else if (strncmp(line, "หา ", strlen("หา ")) == 0) {
             handle_ha(line);
-        } else if (strncmp(line, "รับค่า ", strlen("รับค่า ")) == 0) {
+        } else if (strncmp(line, "รับค่า", strlen("รับค่า")) == 0) {
             handle_rubkum(line);
         }
     }
